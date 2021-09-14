621d89c404e06a8666d6530b6eedf922a3c4583c
Jiri Jancik <jirijancik2@gmail.com>
2021-09-15 01:29:25 +0200

    eslintConfig

--- src/routes/create_invoice/App.tsx
+++ src/routes/create_invoice/App.tsx
@@ -1,31 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import {
-  Form,
-  Input,
-  Button,
-  Select,
-  DatePicker,
-  InputNumber,
-} from 'antd';
-import TextArea from 'antd/lib/input/TextArea';
+import React, { useEffect, useState } from "react";
+import { Form, Input, Button, Select, DatePicker, InputNumber } from "antd";
+import TextArea from "antd/lib/input/TextArea";
+import axios from "axios";
 
- export const CreateInvoice = () => {
+interface Client {
+  name: string;
+  adress: string;
+  ico: string;
+}
 
-  const [data, setData] = useState(" NO DATA ");
+export const CreateInvoice = () => {
+  const [clients, setClients] = useState<Client[]>([]);
+  const [invoices, setInvoices] = useState<any>();
 
   useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+    axios
+      .get("/api/clients")
+      .then(function (response) {
+        setClients(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
   }, []);
 
-  const handleOnFinish = (item:any ) => console.log(item)
+  const handleOnFinish = (item: any) => {
+    axios
+      .get("/api/invoices")
+      .then(function (response) {
+        setInvoices(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+
+    console.log(item);
+  };
 
   return (
     <>
-      <h1>
-        {data}
-      </h1>
       <Form
         onFinish={handleOnFinish}
         labelCol={{
@@ -35,10 +56,17 @@ import TextArea from 'antd/lib/input/TextArea';
           span: 14,
         }}
         layout="horizontal"
+        style={{ marginTop: 50 }}
       >
-
         <Form.Item label="Dodavatel" name="supplier">
-          <Input />
+          <Select placeholder="vzber dodavatele">
+            {clients.length &&
+              clients.map((client) => (
+                <Select.Option key={client.ico} value={client.ico}>
+                  {client.name}: {client.adress}
+                </Select.Option>
+              ))}
+          </Select>
         </Form.Item>
 
         <Form.Item label="Datum prijeti" name="acceptanceDate">
@@ -75,14 +103,30 @@ import TextArea from 'antd/lib/input/TextArea';
 
         <Form.Item label="Kod DPH" name="taxCode">
           <Select>
-            <Select.Option value="one">Prijeti sluzby z tuzemska - zakladni</Select.Option>
-            <Select.Option value="two">Prijeti sluzby ze zahranici - zakladni</Select.Option>
-            <Select.Option value="three">Prijeti sluzby z tuzemska - snizena</Select.Option>
-            <Select.Option value="four">Prijeti sluzby ze zahranici - snizena</Select.Option>
-            <Select.Option value="five">Prijeti zbozi z tuzemska - zakladni</Select.Option>
-            <Select.Option value="six">Prijeti zbozi ze zahranici - zakladni</Select.Option>
-            <Select.Option value="seven">Prijeti zbozi z tuzemska - snizena</Select.Option>
-            <Select.Option value="eight">Prijeti zbozi ze zahranici - snizena</Select.Option>
+            <Select.Option value="one">
+              Prijeti sluzby z tuzemska - zakladni
+            </Select.Option>
+            <Select.Option value="two">
+              Prijeti sluzby ze zahranici - zakladni
+            </Select.Option>
+            <Select.Option value="three">
+              Prijeti sluzby z tuzemska - snizena
+            </Select.Option>
+            <Select.Option value="four">
+              Prijeti sluzby ze zahranici - snizena
+            </Select.Option>
+            <Select.Option value="five">
+              Prijeti zbozi z tuzemska - zakladni
+            </Select.Option>
+            <Select.Option value="six">
+              Prijeti zbozi ze zahranici - zakladni
+            </Select.Option>
+            <Select.Option value="seven">
+              Prijeti zbozi z tuzemska - snizena
+            </Select.Option>
+            <Select.Option value="eight">
+              Prijeti zbozi ze zahranici - snizena
+            </Select.Option>
             <Select.Option value="nine">Nezahrnovat</Select.Option>
           </Select>
         </Form.Item>
@@ -96,7 +140,9 @@ import TextArea from 'antd/lib/input/TextArea';
         </Form.Item>
 
         <Form.Item name="Button">
-          <Button type="primary" htmlType="submit">Odeslat</Button>
+          <Button type="primary" htmlType="submit">
+            Odeslat
+          </Button>
         </Form.Item>
       </Form>
     </>
